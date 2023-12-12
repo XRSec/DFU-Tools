59a3e6687c5bcaa8a4396d67b4e534caf8d65bc7
星冉 <Jalapeno1868@outlook.com>
2023-12-12 17:37:09 +0800

    初始化

---
+++ docs/README.md
@@ -0,0 +1,15 @@
+## macOS DFU Tools
+
+### Download
+
+> [Github Release](https://github.com/XRSec/DFU-Tools/releases)
+> 
+> [镜像加速(Inter 1.1.0)](https://mirror.ghproxy.com/https://github.com/XRSec/DFU-Tools/releases/download/1.1.0/DFU-Tools_x64.dmg)
+> 
+> [镜像加速(Apple 1.1.0)](https://mirror.ghproxy.com/https://github.com/XRSec/DFU-Tools/releases/download/1.1.0/DFU-Tools_arm64.dmg)
+
+### Usage
+
+![dashboard](./dashboard.png)
+
+## 优化
---
+++ electron/index.js
@@ -0,0 +1,106 @@
+const {app, BrowserWindow, ipcMain, Menu, shell} = require('electron')
+const sudo = require('sudo-prompt');
+const {join} = require("node:path");
+const {execSync} = require("child_process");
+const options = {
+    name: "DFU Tools",
+    icns: join(__dirname, 'icon.icns')
+}
+let dfuTools = join(__dirname, 'dfuTools')
+if (__dirname.includes('/Contents/Resources/')) {
+    dfuTools = join(__dirname, '../dfuTools')
+}
+let configuratorStatus = false;
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 920,
+        height: 580,
+        minWidth: 920,
+        minHeight: 580,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            webSecurity: true,
+            devTools: !app.isPackaged, // 如果是开发模式可以使用devTools 调试
+            scrollBounce: process.platform === "darwin", // 在macos中启用橡皮动画
+        },
+        // transparent: true, //设置透明
+    })
+
+    win.loadFile(join(__dirname, 'index.html'))
+    win.on('closed', () => {
+        app.quit();
+    });
+    !app.isPackaged && win.webContents.openDevTools()
+}
+
+Menu.setApplicationMenu(Menu.buildFromTemplate([{
+    label: 'File',
+    submenu: [
+        {role: 'quit'},
+        {role: 'close'},
+    ]
+}]));
+
+app.whenReady().then(() => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+})
+
+ipcMain.on('openReboot', (event) => {
+    if (!checkConfiguratorRunning()) {
+        event.returnValue = "configurator_not_running";
+        return;
+    }
+    sudo.exec(`${dfuTools} reboot`, options, function (error, stdout, _stderr) {
+        if (error) {
+            event.returnValue = "run_error";
+            return
+        }
+        event.returnValue = stdout;
+    })
+})
+
+ipcMain.on('openDFU', (event) => {
+    if (!checkConfiguratorRunning()) {
+        event.returnValue = "configurator_not_running";
+        return;
+    }
+    sudo.exec(`${dfuTools} dfu`, options, function (error, stdout, _stderr) {
+        if (error) {
+            event.returnValue = "run_error";
+            return
+        }
+        event.returnValue = stdout;
+    })
+})
+
+ipcMain.on('openIPSW', (_event) => {
+    const url = 'https://ipsw.me/product/Mac';
+    shell.openExternal(url).then(() => {
+        return "success"
+    }).catch((err) => {
+        return err
+    })
+})
+
+ipcMain.on('openMDM', (_event) => {
+    const url = 'http://mdms.fun';
+    shell.openExternal(url).then(() => {
+        return "success"
+    }).catch((err) => {
+        return err
+    })
+})
+
+function checkConfiguratorRunning() {
+    if (configuratorStatus) return true;
+    const result = execSync('ps -ax | grep -v grep | grep com.apple.configurator; echo', {encoding: 'utf-8'}).toString().trim()
+    configuratorStatus = result !== '';
+    if (configuratorStatus) return true;
+    try {
+        execSync('open -a "Apple Configurator" >/dev/null 2>&1')
+    } catch (e) {
+        console.error(e)
+    }
+    return configuratorStatus;
+}
